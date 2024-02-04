1f31456e8aca1c0a2fe64abb51a0a222caf8e75b
Romeq03 <romekbartek18@gmail.com>
2024-02-04 21:50:19 +0100

    first commit

---
+++ Zadanie 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj nazwę miasta: ");
+        string nazwaMiasta = Console.ReadLine();
+
+        Console.Write("Podaj nazwę województwa: ");
+        string nazwaWojewodztwa = Console.ReadLine();
+
+        int dlugoscMiasta = nazwaMiasta.Length;
+        int dlugoscWojewodztwa = nazwaWojewodztwa.Length;
+
+        Console.WriteLine($"Nazwa miasta {nazwaMiasta} zawiera {dlugoscMiasta} liter.");
+        Console.WriteLine($"Nazwa województwa {nazwaWojewodztwa} zawiera {dlugoscWojewodztwa} liter.");
+
+        Console.ReadLine();
+    }
+}
